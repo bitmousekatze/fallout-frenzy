ffdf9f63f6b7e4b4c5f6cecbaa7e0692058b2247
bitmousekatze <bitmousekatze@gmail.com>
2026-05-06 17:38:41 +0200

    Pushed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fallout-frenzy/",
   server: {
     host: "::",
     port: 8080,
